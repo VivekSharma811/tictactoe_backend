caa1a3aa3b1bdce3e27ae962d3885a86409ec037
Vivek <vivek.sharma@mpokket.com>
2023-08-17 18:01:58 +0530

    Setup added

---
+++ src/main/kotlin/com/hypheno/Application.kt
@@ -0,0 +1,15 @@
+package com.hypheno
+
+import com.hypheno.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSockets()
+    configureSerialization()
+    configureMonitoring()
+    configureRouting()
+}
